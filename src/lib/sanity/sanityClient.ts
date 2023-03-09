6160e6f2ca7f62d1ad77d9f72c5c791ca131f062
hy-fy <hollyalexisfogarty@gmail.com>
2023-03-09 14:17:09 -0500

    await createclient

--- src/lib/sanity/sanityClient.ts
+++ src/lib/sanity/sanityClient.ts
@@ -9,7 +9,7 @@ const config = {
 	// token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
 };
 
-export const client = createClient(config);
+export const client = await createClient(config);
 
 export const query = async (query: string) => await client.fetch(query);
 
