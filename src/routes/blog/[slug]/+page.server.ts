14cfed57997fd81a469377236c282f61d10c1236
hy-fy <hollyalexisfogarty@gmail.com>
2023-03-09 20:09:56 -0500

    Updating to server side rendered ts

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,12 +1,12 @@
-import { query } from '$lib/sanity/sanityClient';
-import type { PageLoad } from './$types';
+import { query } from '$lib/sanity/+server';
+//import type { PageLoad } from './$types';
 import { marked } from 'marked';
 
 // since there's no dynamic data here, we can prerender
 // it so that it gets served as a static asset in production
 export const prerender = true;
 
-export const load: PageLoad = async ({ fetch, params }) => {
+export const load = async ({ fetch, params }) => {
 	const slug = params['slug'];
 	const args = `*[slug.current == '${slug}'][0]{
   _id, title, author, content,
